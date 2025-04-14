60834336be1385dd1a586e52c236105ebe06bbf6
alimirhashimli1 <alimirhashimli@gmail.com>
2025-04-14 02:19:06 +0200

    added more routers

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -6,6 +6,28 @@ app.MapGet("/downloads", () => "Downloads!");
 app.MapPut("/", () => "This is a put!");
 app.MapDelete("/", () => "This is a delete!");
 app.MapPost("/", () => "This is a POst!");
+app.MapGet("/users/{userId}/posts/{slug}", (int userId, string slug) => {
+    return $"User id: {userId}, Post ID: {slug}";
+});
+app.MapGet("/products/{id:int:min(0)}", (int id) => {
+    return $"Product id: {id}";
+});
 
+app.MapGet("/products/{category}/{id}/{year?}", (string category,  int id, int? year) => {
+        var cleanCategory = category.Replace("-", " ");
+    return $" This {cleanCategory} is produced in {year}. Product id: {id}";
+});
+
+app.MapGet("/files/{*filePath}", (string filePath) => {
+    return $"File path: {filePath}";
+});
+
+app.MapGet("/search", (string? q, int page =1) => {
+    return $"Searching for {q} on page {page}";
+});
+
+app.MapGet("/store/{category}/{productId:int?}/{*extraPath}", (string category, int? productId, string? extraPath, bool inStock = true) => {
+    return $"{category} {productId} {extraPath} {inStock}";
+});
 app.Run();
  
